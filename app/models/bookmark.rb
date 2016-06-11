77fd7a1ac7ee74176241bf39253e3fc91199e1a0
Andreas Philippi <anphil@users.noreply.github.com>
2016-06-11 12:05:27 +0200

    Add associations and validations to the models

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,2 +1,5 @@
 class Bookmark < ActiveRecord::Base
+  has_and_belongs_to_many :tags
+  validates :title, presence: true, allow_blank: false 
+  validates :url, presence: true, allow_blank: false
 end
