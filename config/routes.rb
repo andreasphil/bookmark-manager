16002a5ce5dc65439df6558a01d0be86a703f5a6
Samuel Portz <sportz@users.noreply.github.com>
2016-06-15 14:10:50 +0200

    Add :confirmable to User

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
 
   resources :user_profiles
+  resources :tags
+  resources :bookmarks, except: :show
+
   devise_for :users, controllers: {
         registrations: 'users/registrations'
       }
-
-  resources :tags
-  resources :bookmarks, except: :show
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
