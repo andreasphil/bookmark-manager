b947b77667f0bac2ca08ea19f49b5c14167b758d
Andreas Philippi <anphil@users.noreply.github.com>
2016-06-15 17:35:53 +0200

    Adapt generated tests to Devise and the changes in models, controllers and routes

--- test/controllers/user_profiles_controller_test.rb
+++ test/controllers/user_profiles_controller_test.rb
@@ -1,32 +1,17 @@
 require 'test_helper'
 
 class UserProfilesControllerTest < ActionController::TestCase
+  include Devise::TestHelpers
+
   setup do
     @user_profile = user_profiles(:one)
-  end
-
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:user_profiles)
-  end
-
-  test "should get new" do
-    get :new
-    assert_response :success
+    sign_in users(:one)
   end
 
   test "should create user_profile" do
     assert_difference('UserProfile.count') do
-      post :create, user_profile: { backgound_image: @user_profile.backgound_image, user_id: @user_profile.user_id }
+      post :create, user_profile: { background_image: @user_profile.background_image, user_id: @user_profile.user_id }
     end
-
-    assert_redirected_to user_profile_path(assigns(:user_profile))
-  end
-
-  test "should show user_profile" do
-    get :show, id: @user_profile
-    assert_response :success
   end
 
   test "should get edit" do
@@ -35,15 +20,12 @@ class UserProfilesControllerTest < ActionController::TestCase
   end
 
   test "should update user_profile" do
-    patch :update, id: @user_profile, user_profile: { backgound_image: @user_profile.backgound_image, user_id: @user_profile.user_id }
-    assert_redirected_to user_profile_path(assigns(:user_profile))
+    patch :update, id: @user_profile, user_profile: { background_image: @user_profile.background_image, user_id: @user_profile.user_id }
   end
 
   test "should destroy user_profile" do
     assert_difference('UserProfile.count', -1) do
       delete :destroy, id: @user_profile
     end
-
-    assert_redirected_to user_profiles_path
   end
 end
