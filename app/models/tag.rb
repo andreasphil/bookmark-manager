d068d20ee8e6947e8b021bdadbb28610ed5b2cf7
Samuel Portz <sportz@users.noreply.github.com>
2016-06-15 13:11:07 +0200

    Remove global uniqueness of Tag.title Add user uniqueness of Bookmark.url

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,5 @@
 class Tag < ActiveRecord::Base
   has_and_belongs_to_many :bookmarks
   belongs_to :user
-  validates :title, presence: true, uniqueness: true, allow_blank: false
+  validates :title, presence: true, uniqueness: { scope: :user, message: 'already exists' }, allow_blank: false
 end
