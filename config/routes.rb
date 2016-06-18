fb1b6a1f027fefe9fa511b5e0104a4597c110493
Samuel Portz <sportz@users.noreply.github.com>
2016-06-18 16:22:08 +0200

    Redirect to resend confirmation when attempting to login in with not confirmed account

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   resources :bookmarks, except: :show
   
   devise_for :users, controllers: {
-        registrations: 'users/registrations'
+        registrations: 'users/registrations',
+        sessions: 'users/sessions'
       }
 
   # The priority is based upon order of creation: first created -> highest priority.
