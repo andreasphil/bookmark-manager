cfde372e87ee85fe2095827e733122bef323f65b
Samuel Portz <sportz@users.noreply.github.com>
2016-06-19 22:26:55 +0200

    Change mailer host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Devise default mailer url
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'manage-my-bookmarks.herokuapp.com', port: 3000 }
 end
