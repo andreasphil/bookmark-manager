8fa7db1b7f20a438acb9a60d70219dba1a16a7ef
Andreas Philippi <anphil@users.noreply.github.com>
2016-06-21 13:17:59 +0200

    Include background in application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :set_background_url
 
   # Devise redirection URLs
   def after_sign_in_path_for(user)
@@ -15,5 +16,11 @@ class ApplicationController < ActionController::Base
   # Store previous page
   def store_location
     store_location_for :user, request.referer
+  end
+
+  def set_background_url
+    if user_signed_in?
+      @background_url = current_user.user_profile.background_image.url
+    end
   end
 end
